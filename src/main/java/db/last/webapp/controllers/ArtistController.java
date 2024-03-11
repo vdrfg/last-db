a74ec14dba543fcce69a4dd9433b6241abae9529
Jan Benda <bendajan@centrum.cz>
2024-03-11 14:37:50 +0100

    added edit artist endpoints

--- src/main/java/db/last/webapp/controllers/ArtistController.java
+++ src/main/java/db/last/webapp/controllers/ArtistController.java
@@ -1,6 +1,7 @@
 package db.last.webapp.controllers;
 
 import db.last.webapp.dtos.ArtistInDTO;
+import db.last.webapp.dtos.ArtistOutDTO;
 import db.last.webapp.models.Artist;
 import db.last.webapp.services.ArtistService;
 import jakarta.validation.Valid;
@@ -15,35 +16,55 @@ import java.util.Optional;
 @RequestMapping("/artists")
 @AllArgsConstructor
 public class ArtistController {
-    private ArtistService artistService;
+  private ArtistService artistService;
 
-    @GetMapping
-    public String allArtists(Model model) {
-        model.addAttribute("artists", artistService.getAllArtists());
-        return "artists";
-    }
+  @GetMapping
+  public String allArtists(Model model) {
+    model.addAttribute("artists", artistService.getAllArtists());
+    return "artists";
+  }
 
-    @GetMapping("/{name}")
-    public String artist(@PathVariable String name, Model model) {
-        Optional<Artist> optArtist = artistService.getArtist(name);
-        if (optArtist.isEmpty()) {
-            //TODO: create dynamic error page
-            model.addAttribute("error", "error");
-            return "artists";
-        }
-        model.addAttribute("artist", optArtist.get());
-        return "/artist/detail";
+  @GetMapping("/{name}")
+  public String artist(@PathVariable String name, Model model) {
+    Optional<Artist> optArtist = artistService.getArtist(name);
+    if (optArtist.isEmpty()) {
+      // TODO: create dynamic error page
+      model.addAttribute("error", "error");
+      return "artists";
     }
+    model.addAttribute("artist", new ArtistOutDTO(optArtist.get()));
+    return "/artist/detail";
+  }
 
-    @GetMapping("/new")
-    public String create(Model model) {
-        model.addAttribute("artist", new Artist());
-        return "artist/form";
-    }
+  @GetMapping("/new")
+  public String create(Model model) {
+    model.addAttribute("artist", new Artist());
+    return "artist/form";
+  }
 
-    @PostMapping
-    public String create(@Valid ArtistInDTO artistInDTO) {
-        Artist artist = artistService.save(artistInDTO);
-        return String.format("redirect:/artists/%s", artist.getName());
+  @PostMapping
+  public String create(@Valid ArtistInDTO artistInDTO) {
+    Artist artist = artistService.save(artistInDTO);
+    return String.format("redirect:/artists/%s", artist.getName());
+  }
+
+  @GetMapping("/{name}/edit")
+  public String edit(@PathVariable String name, Model model) {
+    Optional<Artist> optArtist = artistService.getArtist(name);
+    if (optArtist.isPresent()) {
+      model.addAttribute("artist", new ArtistOutDTO(optArtist.get()));
+      return "artist/form";
     }
+    return "error";
+  }
+
+  @PutMapping("/{name}")
+  public String edit(@PathVariable String name, ArtistInDTO artistDTO) {
+    Optional<Artist> optArtist = artistService.getArtist(name);
+    if (optArtist.isPresent()) {
+      // TODO: add editing method
+      return String.format("redirect:/artists/%s", optArtist.get().getName());
+    }
+    return "error";
+  }
 }
--- src/main/java/db/last/webapp/controllers/MainController.java
+++ src/main/java/db/last/webapp/controllers/MainController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @AllArgsConstructor
 public class MainController {
 
-    @GetMapping
-    public String index() {
-        return "index";
-    }
+  @GetMapping
+  public String index() {
+    return "index";
+  }
 }
