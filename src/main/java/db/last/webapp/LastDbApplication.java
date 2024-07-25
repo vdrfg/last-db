3c63f3b3540042a38fd8f83adacba66c058dd918
Jan Benda <bendajan@centrum.cz>
2024-07-25 15:13:10 +0200

    initial commit

---
+++ src/main/java/db/last/webapp/LastDbApplication.java
@@ -0,0 +1,13 @@
+package db.last.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/db/last/webapp/LastDbApplicationTests.java
@@ -0,0 +1,13 @@
+package db.last.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
